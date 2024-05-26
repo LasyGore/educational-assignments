f8aa7c14d93945d27e49f6bcf6bce8a941341e69
LasyGore <163201245+LasyGore@users.noreply.github.com>
2024-05-27 01:24:45 +0300

    Add files via upload

---
+++ main_27_05.py
@@ -0,0 +1,22 @@
+# декараторы
+def is_prime(func):
+    def wrapper(*args):
+        result = func(*args)
+        if result > 1:
+            for i in range(2, int(result**0.5) + 1):
+                if result % i == 0:
+                    print("Составное")
+                    break
+            else:
+                print("Простое")
+        return result
+    return wrapper
+
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+
+res = sum_three(5, 7, 11)
+print(res)
